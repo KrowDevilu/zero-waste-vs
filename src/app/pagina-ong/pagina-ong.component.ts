bcfe21a396a01ff1ed8008f600695ea5fbe8d20e
Lucas Lima Souza <104106664+KrowDevilu@users.noreply.github.com>
2023-05-22 21:35:25 -0300

    Add files via upload

--- src/app/pagina-ong/pagina-ong.component.ts
+++ src/app/pagina-ong/pagina-ong.component.ts
@@ -1,5 +1,6 @@
 import {  AfterViewInit, Component } from '@angular/core';
 import { OnInit } from '@angular/core';
+import { HammerGestureConfig } from '@angular/platform-browser';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import {ongs} from '../ongs';
@@ -13,7 +14,19 @@ export class PaginaOngComponent implements AfterViewInit{
   constructor(private route: ActivatedRoute) { }
   percentage = 12;
   ongs = ongs;
-  
+  heart = { outline: "../../assets/heart-outline.png", full: "../../assets/heart-full.png"}
+  fav_src = this.heart.outline;
+  favved = false
+  updateFav(){
+    if (this.favved == false){
+      this.fav_src = this.heart.full;
+      this.favved = true;
+    }
+    else{
+      this.fav_src = this.heart.outline;
+      this.favved = false;
+    }
+  }
   ngAfterViewInit() {
     const myElement = document.getElementById("progress") as HTMLElement;
     myElement.style.width = `${this.percentage}%`;
@@ -26,5 +39,7 @@ export class PaginaOngComponent implements AfterViewInit{
     this.route.paramMap.subscribe((params: ParamMap) => {
       // @ts-ignore: Object is possibly 'null'.
       this.id = +params.get('id');
-    });}
+    });
+  }
+
 }
